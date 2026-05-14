220bf210f7550b51f1da1af731889d750d7e3eea
Dante Stivaktas <stivaktasd@gmail.com>
2026-05-14 00:25:36 -0600

    Update 11-schools_by_topic.py

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python3
-""" Returns the list of school having a specific topic """
-
-
 def schools_by_topic(mongo_collection, topic):
-    """
-    Returns the list of school having a specific topic
-    """
     return list(mongo_collection.find({"topics": topic}))
