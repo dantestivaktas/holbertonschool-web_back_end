1fa1052c6226e4a276b80769b056ab3e533e8506
Dante Stivaktas <stivaktasd@gmail.com>
2026-05-14 00:23:02 -0600

    Update 7-average_score.sql

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,12 +1,8 @@
--- Script that creates a stored procedure ComputeAverageScoreForUser
--- that computes and store the average score for a student.
-
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    -- Update the average_score in the users table by computing the average
-    -- of all scores for the specific user in the corrections table.
+    
     UPDATE users
     SET average_score = (
         SELECT AVG(score)
